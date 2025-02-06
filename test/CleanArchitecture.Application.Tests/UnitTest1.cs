8639ff7ed33f5e0ea794cecf7fa3cb6b83ba2560
alirezaxcs <alireza.xcs@gmail.com>
2025-02-06 16:52:10 +0100

    initilize

---
+++ test/CleanArchitecture.Application.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace CleanArchitecture.Application.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ test/CleanArchitecture.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace CleanArchitecture.Domain.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
