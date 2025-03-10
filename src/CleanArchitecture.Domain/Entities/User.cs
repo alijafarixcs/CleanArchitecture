0ca9bc9218ff6027141ccecf021eb7851c98eb71
alirezaxcs <alireza.xcs@gmail.com>
2025-03-10 12:56:19 +0100

    Initialize project

---
+++ src/CleanArchitecture.Domain/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿namespace CleanArchitecture.Domain;
+
+public class User
+{
+    public Guid Id { get; set; }=Guid.NewGuid();
+    public string Name { get; set; } = null;
+    public string LastName { get; set; }
+    public string Email { get; set; } = null;
+    public string Password { get; set; } = null;
+
+}
